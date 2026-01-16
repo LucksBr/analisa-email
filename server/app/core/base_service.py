e497bdeac6eaf553cbf1f81a16182c6f17f04457
Lucas Do Prado <93302128+LucksBr@users.noreply.github.com>
2026-01-16 12:06:12 -0300

    Creating validate in BaseService and hooks before e after of save and update

--- server/app/core/base_service.py
+++ server/app/core/base_service.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from typing import Generic, TypeVar, List, Optional
 from server.app.core.base_repository import BaseRepository
 
@@ -14,12 +15,38 @@ class BaseService(Generic[T,ID]):
     
     def get_by_id(self, entity_id: ID) -> Optional[T]:
         return self.repository.find_by_id(entity_id)
+    
+    @abstractmethod
+    def validate(self, entity: T) -> None:
+        pass
+
+    def _before_create(self, entity: T) -> T:
+        return entity
+    
+    def _after_create(self, entity: T) -> T:
+        return entity
 
     def create(self, entity: T) -> T:
-        return self.repository.merge(entity)
+        entity = self._before_create(entity)
+        self.validate(entity)
+        entity = self.repository.merge(entity)
+        entity = self._after_create(entity)
+
+        return entity
+    
+    def _before_update(self, entity: T) -> T:
+        return entity
+    
+    def _after_update(self, entity: T) -> T:
+        return entity
     
     def update(self, entity: T) -> T:
-        return self.repository.merge(entity)
+        entity = self._before_update(entity)
+        self.validate(entity)
+        entity = self.repository.merge(entity)
+        entity = self._after_update(entity)
+
+        return entity
 
     def delete(self, entity: T) -> None:
         self.repository.delete(entity)
