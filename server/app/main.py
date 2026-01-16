21246b837c3e2fb347f56d1818e8acc2db8eebcc
Lucas Do Prado <93302128+LucksBr@users.noreply.github.com>
2026-01-16 09:50:21 -0300

    Calling the registers for exceptions and routers in the main file.

--- server/app/main.py
+++ server/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from server.app.exception.registry_exception import register_exceptions_handlers
 from server.app.config.database import create_db_and_tables
-from server.app.controller.user_controller import router as user_router
+from server.app.controller.router.registry_router import register_routers
 
 app = FastAPI()
 
@@ -8,4 +9,6 @@ app = FastAPI()
 def on_startup():
     create_db_and_tables()
 
-app.include_router(user_router)
+# Resistro de handlers de exceções e rotas da Api
+register_exceptions_handlers(app)
+register_routers(app)
